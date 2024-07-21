431db64094c940d95d7f95551446627d560b678d
Vu Minh Dang <133772077+dngvmnh@users.noreply.github.com>
2024-07-21 19:35:14 +0700

    update

--- src/trainer/pose/offline_tts.py
+++ src/trainer/pose/offline_tts.py
@@ -1,4 +1,4 @@
-import pyttsx3
+# import pyttsx3
 # engine = pyttsx3.init()
 # engine.say("I will speak this text")
 # engine.runAndWait()
@@ -8,27 +8,26 @@ import pyttsx3
 
 
 
-# import pyttsx3
-# engine = pyttsx3.init() # object creation
+import pyttsx3
+engine = pyttsx3.init() # object creation
 
-# """ RATE"""
-# rate = engine.getProperty('rate')   # getting details of current speaking rate
-# engine.setProperty('rate', 150)     # setting up new voice rate
+""" RATE"""
+rate = engine.getProperty('rate')   # getting details of current speaking rate
+engine.setProperty('rate', 150)     # setting up new voice rate
 
 
-# """VOLUME"""
-# volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
-# engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
+"""VOLUME"""
+volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
+engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
 
-# """VOICE"""
-# voices = engine.getProperty('voices')       #getting details of current voice
-# # engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
-# engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
+"""VOICE"""
+voices = engine.getProperty('voices')       #getting details of current voice
+engine.setProperty('voice', voices[0].id)  
+# engine.setProperty('voice', voices[1].id)   
 
-# engine.say("Tao là Tùng béo")
-# engine.say('Pho len ti lô Rô nan đô siu!')
-# engine.runAndWait()
-# engine.stop()
+engine.say("Hello World!")
+engine.runAndWait()
+engine.stop()
 
 
 # """Saving Voice to a file"""
@@ -115,8 +114,9 @@ import pyttsx3
 
 
 
-engine = pyttsx3.init()
-engine.say('The quick brown fox jumped over the lazy dog.', 'fox')
-engine.startLoop(False)
-# engine.iterate() must be called inside externalLoop()
-engine.endLoop()
+# engine = pyttsx3.init()
+# engine.say('The quick brown fox jumped over the lazy dog.', 'fox')
+# engine.startLoop(False)
+# # engine.iterate() must be called inside externalLoop()
+# # externalLoop()
+# engine.endLoop()
